604f892c25ccc5239b28aabbc916e778514f2da7
Cheng <lichchchn@gmail.com>
2020-08-20 10:01:30 +0800

    format send and recv cpp code

--- package/recvCSI/src/csi_fun.h
+++ package/recvCSI/src/csi_fun.h
@@ -13,42 +13,42 @@
  * =====================================================================================
  */
 #include <stdbool.h>
-#define Kernel_CSI_ST_LEN 23 
+#define Kernel_CSI_ST_LEN 23
 typedef struct
 {
     int real;
     int imag;
-}COMPLEX;
+} COMPLEX;
 
 typedef struct
 {
-    uint64_t tstamp;         /* h/w assigned time stamp */
-    
-    uint16_t channel;        /* wireless channel (represented in Hz)*/
-    uint8_t  chanBW;         /* channel bandwidth (0->20MHz,1->40MHz)*/
+    uint64_t tstamp; /* h/w assigned time stamp */
 
-    uint8_t  rate;           /* transmission rate*/
-    uint8_t  nr;             /* number of receiving antenna*/
-    uint8_t  nc;             /* number of transmitting antenna*/
-    uint8_t  num_tones;      /* number of tones (subcarriers) */
-    uint8_t  noise;          /* noise floor (to be updated)*/
+    uint16_t channel; /* wireless channel (represented in Hz)*/
+    uint8_t chanBW;   /* channel bandwidth (0->20MHz,1->40MHz)*/
 
-    uint8_t  phyerr;          /* phy error code (set to 0 if correct)*/
+    uint8_t rate;      /* transmission rate*/
+    uint8_t nr;        /* number of receiving antenna*/
+    uint8_t nc;        /* number of transmitting antenna*/
+    uint8_t num_tones; /* number of tones (subcarriers) */
+    uint8_t noise;     /* noise floor (to be updated)*/
 
-    uint8_t    rssi;         /*  rx frame RSSI */
-    uint8_t    rssi_0;       /*  rx frame RSSI [ctl, chain 0] */
-    uint8_t    rssi_1;       /*  rx frame RSSI [ctl, chain 1] */
-    uint8_t    rssi_2;       /*  rx frame RSSI [ctl, chain 2] */
+    uint8_t phyerr; /* phy error code (set to 0 if correct)*/
 
-    uint16_t   payload_len;  /*  payload length (bytes) */
-    uint16_t   csi_len;      /*  csi data length (bytes) */
-    uint16_t   buf_len;      /*  data length in buffer */
-}csi_struct;
+    uint8_t rssi;   /*  rx frame RSSI */
+    uint8_t rssi_0; /*  rx frame RSSI [ctl, chain 0] */
+    uint8_t rssi_1; /*  rx frame RSSI [ctl, chain 1] */
+    uint8_t rssi_2; /*  rx frame RSSI [ctl, chain 2] */
 
-bool  is_big_endian();
-int   open_csi_device();
-void  close_csi_device(int fd);
-int   read_csi_buf(unsigned char* buf_addr,int fd, int BUFSIZE);
-void  record_status(unsigned char* buf_addr, int cnt, csi_struct* csi_status);
-void  record_csi_payload(unsigned char* buf_addr, csi_struct* csi_status,unsigned char* data_buf, COMPLEX(* csi_buf)[3][114]);
-void  porcess_csi(unsigned char* data_buf, csi_struct* csi_status,COMPLEX(* csi_buf)[3][114]);
+    uint16_t payload_len; /*  payload length (bytes) */
+    uint16_t csi_len;     /*  csi data length (bytes) */
+    uint16_t buf_len;     /*  data length in buffer */
+} csi_struct;
+
+bool is_big_endian();
+int open_csi_device();
+void close_csi_device(int fd);
+int read_csi_buf(unsigned char *buf_addr, int fd, int BUFSIZE);
+void record_status(unsigned char *buf_addr, int cnt, csi_struct *csi_status);
+void record_csi_payload(unsigned char *buf_addr, csi_struct *csi_status, unsigned char *data_buf, COMPLEX (*csi_buf)[3][114]);
+void porcess_csi(unsigned char *data_buf, csi_struct *csi_status, COMPLEX (*csi_buf)[3][114]);
--- package/sendData/src/sendData.c
+++ package/sendData/src/sendData.c
@@ -15,7 +15,7 @@
  *  (at your option) any later version.
  * =====================================================================================
  */
- 
+
 #include <arpa/inet.h>
 #include <linux/if_packet.h>
 #include <stdio.h>
@@ -33,36 +33,36 @@
 #include <errno.h>
 
 /* Define the defult destination MAC address */
-#define DEFAULT_DEST_MAC0	0x00
-#define DEFAULT_DEST_MAC1	0x03
-#define DEFAULT_DEST_MAC2	0x7F
-#define DEFAULT_DEST_MAC3	0xB0
-#define DEFAULT_DEST_MAC4	0x20
-#define DEFAULT_DEST_MAC5	0x20
- 
-#define DEFAULT_IF	        "wlan0"
-#define BUF_SIZ	            2048	
- 
+#define DEFAULT_DEST_MAC0 0x00
+#define DEFAULT_DEST_MAC1 0x03
+#define DEFAULT_DEST_MAC2 0x7F
+#define DEFAULT_DEST_MAC3 0xB0
+#define DEFAULT_DEST_MAC4 0x20
+#define DEFAULT_DEST_MAC5 0x20
+
+#define DEFAULT_IF "wlan0"
+#define BUF_SIZ 2048
+
 int main(int argc, char *argv[])
 {
-	int     sockfd;
-    	int     i;
-	struct  ifreq if_idx;
-	struct  ifreq if_mac;
-	int     tx_len = 0,Cnt;
-	char    sendbuf[BUF_SIZ];
-    	unsigned int DstAddr[6];
-	struct  ether_header *eh = (struct ether_header *) sendbuf;
-	struct  iphdr *iph = (struct iphdr *) (sendbuf + sizeof(struct ether_header));
-	struct  sockaddr_ll socket_address;
-	char    ifName[IFNAMSIZ];
-	
-    if (argc == 1)
-    {
-        printf("Usage:   %s ifName DstMacAddr NumOfPacketToSend\n",argv[0]);
-        printf("Example: %s wlan0 00:7F:5D:3E:4A 100\n",argv[0]);
-        exit(0);
-    }
+	int sockfd;
+	int i;
+	struct ifreq if_idx;
+	struct ifreq if_mac;
+	int tx_len = 0, Cnt;
+	char sendbuf[BUF_SIZ];
+	unsigned int DstAddr[6];
+	struct ether_header *eh = (struct ether_header *)sendbuf;
+	struct iphdr *iph = (struct iphdr *)(sendbuf + sizeof(struct ether_header));
+	struct sockaddr_ll socket_address;
+	char ifName[IFNAMSIZ];
+
+	if (argc == 1)
+	{
+		printf("Usage:   %s ifName DstMacAddr NumOfPacketToSend\n", argv[0]);
+		printf("Example: %s wlan0 00:7F:5D:3E:4A 100\n", argv[0]);
+		exit(0);
+	}
 
 	/* Get interface name */
 	if (argc > 1)
@@ -70,44 +70,44 @@ int main(int argc, char *argv[])
 	else
 		strcpy(ifName, DEFAULT_IF);
 
-    //dst address seperated by :, example: 00:7F:5D:3E:4A
-    if(argc>2)
-    {
-        sscanf(argv[2],"%x:%x:%x:%x:%x:%x",&DstAddr[0],&DstAddr[1],&DstAddr[2],&DstAddr[3],&DstAddr[4],&DstAddr[5]);
-        //printf("DstMacAddr: %02x:%02x:%02x:%02x:%02x:%02x\n",DstAddr[0],DstAddr[1],DstAddr[2],DstAddr[3],DstAddr[4],DstAddr[5]);
-    }
-    else
-    {
-        DstAddr[0] = DEFAULT_DEST_MAC0;
-        DstAddr[1] = DEFAULT_DEST_MAC1;
-        DstAddr[2] = DEFAULT_DEST_MAC2;
-        DstAddr[3] = DEFAULT_DEST_MAC3;
-        DstAddr[4] = DEFAULT_DEST_MAC4;
-        DstAddr[5] = DEFAULT_DEST_MAC5;
-    }
-
-    if(argc > 3)
-        Cnt = atoi(argv[3]);
-    else
-        Cnt = 1;
-	
- 
-	/* Open RAW socket to send on */
-	if ((sockfd = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL))) == -1) {
-	    perror("socket");
+	//dst address seperated by :, example: 00:7F:5D:3E:4A
+	if (argc > 2)
+	{
+		sscanf(argv[2], "%x:%x:%x:%x:%x:%x", &DstAddr[0], &DstAddr[1], &DstAddr[2], &DstAddr[3], &DstAddr[4], &DstAddr[5]);
+		//printf("DstMacAddr: %02x:%02x:%02x:%02x:%02x:%02x\n",DstAddr[0],DstAddr[1],DstAddr[2],DstAddr[3],DstAddr[4],DstAddr[5]);
 	}
- 
+	else
+	{
+		DstAddr[0] = DEFAULT_DEST_MAC0;
+		DstAddr[1] = DEFAULT_DEST_MAC1;
+		DstAddr[2] = DEFAULT_DEST_MAC2;
+		DstAddr[3] = DEFAULT_DEST_MAC3;
+		DstAddr[4] = DEFAULT_DEST_MAC4;
+		DstAddr[5] = DEFAULT_DEST_MAC5;
+	}
+
+	if (argc > 3)
+		Cnt = atoi(argv[3]);
+	else
+		Cnt = 1;
+
+	/* Open RAW socket to send on */
+	if ((sockfd = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL))) == -1)
+	{
+		perror("socket");
+	}
+
 	/* Get the index of the interface to send on */
 	memset(&if_idx, 0, sizeof(struct ifreq));
-	strncpy(if_idx.ifr_name, ifName, IFNAMSIZ-1);
+	strncpy(if_idx.ifr_name, ifName, IFNAMSIZ - 1);
 	if (ioctl(sockfd, SIOCGIFINDEX, &if_idx) < 0)
-	    perror("SIOCGIFINDEX");
+		perror("SIOCGIFINDEX");
 	/* Get the MAC address of the interface to send on */
 	memset(&if_mac, 0, sizeof(struct ifreq));
-	strncpy(if_mac.ifr_name, ifName, IFNAMSIZ-1);
+	strncpy(if_mac.ifr_name, ifName, IFNAMSIZ - 1);
 	if (ioctl(sockfd, SIOCGIFHWADDR, &if_mac) < 0)
-	    perror("SIOCGIFHWADDR");
- 
+		perror("SIOCGIFHWADDR");
+
 	/* Construct the Ethernet header */
 	memset(sendbuf, 0, BUF_SIZ);
 	/* Ethernet header */
@@ -124,34 +124,35 @@ int main(int argc, char *argv[])
 	eh->ether_dhost[4] = DstAddr[4];
 	eh->ether_dhost[5] = DstAddr[5];
 
-    /* Ethertype field */
+	/* Ethertype field */
 	eh->ether_type = htons(ETH_P_IP);
 	tx_len += sizeof(struct ether_header);
- 
+
 	/* Packet data 
      * We just set it to 0xaa you send arbitrary payload you like*/
-    for(i=1;i<=1000;i++){
-        
-	    sendbuf[tx_len++] = 0xaa;
-    } 
-    printf("Packet Length is: %d,pkt_num is: %d\n",tx_len,Cnt); 
-	
-    /* Index of the network device */
+	for (i = 1; i <= 1000; i++)
+	{
+
+		sendbuf[tx_len++] = 0xaa;
+	}
+	printf("Packet Length is: %d,pkt_num is: %d\n", tx_len, Cnt);
+
+	/* Index of the network device */
 	socket_address.sll_ifindex = if_idx.ifr_ifindex;
-    /* RAW communication*/
-    socket_address.sll_family   = PF_PACKET;    
-    /* we don't use a protocoll above ethernet layer
+	/* RAW communication*/
+	socket_address.sll_family = PF_PACKET;
+	/* we don't use a protocoll above ethernet layer
      *   ->just use anything here*/
-    socket_address.sll_protocol = htons(ETH_P_IP);  
-    
-    /* ARP hardware identifier is ethernet*/
-    socket_address.sll_hatype   = ARPHRD_ETHER;
-        
-    /* target is another host*/
-    socket_address.sll_pkttype  = PACKET_OTHERHOST;
-    
-    /* address length*/
-    socket_address.sll_halen    = ETH_ALEN;
+	socket_address.sll_protocol = htons(ETH_P_IP);
+
+	/* ARP hardware identifier is ethernet*/
+	socket_address.sll_hatype = ARPHRD_ETHER;
+
+	/* target is another host*/
+	socket_address.sll_pkttype = PACKET_OTHERHOST;
+
+	/* address length*/
+	socket_address.sll_halen = ETH_ALEN;
 	/* Destination MAC */
 	socket_address.sll_addr[0] = DstAddr[0];
 	socket_address.sll_addr[1] = DstAddr[1];
@@ -159,21 +160,23 @@ int main(int argc, char *argv[])
 	socket_address.sll_addr[3] = DstAddr[3];
 	socket_address.sll_addr[4] = DstAddr[4];
 	socket_address.sll_addr[5] = DstAddr[5];
- 
+
 	/* Send packet */
-    for(;Cnt>0;Cnt--)
-    {
-        /* you set the time interval between two transmitting packets 
+	for (; Cnt > 0; Cnt--)
+	{
+		/* you set the time interval between two transmitting packets 
          * for example, here we set it to 50 microseconds
          * set to 0 if you don't need it
          */
-        if (usleep(50) == -1){
-            printf("sleep failed\n");
-        }
-        if (sendto(sockfd, sendbuf, tx_len, 0, (struct sockaddr*)&socket_address, sizeof(struct sockaddr_ll)) < 0){
-            printf("Send failed\n");
-        }
-    }
-	
+		if (usleep(50) == -1)
+		{
+			printf("sleep failed\n");
+		}
+		if (sendto(sockfd, sendbuf, tx_len, 0, (struct sockaddr *)&socket_address, sizeof(struct sockaddr_ll)) < 0)
+		{
+			printf("Send failed\n");
+		}
+	}
+
 	return 0;
 }
